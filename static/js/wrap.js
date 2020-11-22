977f645068fd55a6035f1f3ecc2cc4ea96a604af
John McLear <john@mclear.co.uk>
2020-11-22 18:29:03 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/wrap.js
+++ static/js/wrap.js
@@ -1,76 +1,76 @@
 // Sorry but we need wrap to be exposed so we can access it
 var wrap;
 
-exports.acePostWriteDomLineHTML = function(callstack, editorInfo, rep, documentAttributeManager){
-  if(wrap && wrap.status() === false){ // if wrapping is disabled
+exports.acePostWriteDomLineHTML = function (callstack, editorInfo, rep, documentAttributeManager) {
+  if (wrap && wrap.status() === false) { // if wrapping is disabled
     wrap.updateUI();
   }
-}
+};
 
 // Required to bring page back into focus when caret hits first char
-exports.aceEditEvent = function(hook, call){
+exports.aceEditEvent = function (hook, call) {
   // If it's not a click or a key event
-  var cs = call.callstack;
-  if(!(cs.type == "handleClick") && !(cs.type == "handleKeyEvent")){
+  const cs = call.callstack;
+  if (!(cs.type == 'handleClick') && !(cs.type == 'handleKeyEvent')) {
     return false;
   }
   // If it's an initial setup event then do nothing..
-  if(cs.type == "setBaseText" || cs.type == "setup") return false;
+  if (cs.type == 'setBaseText' || cs.type == 'setup') return false;
   // A bug exists here where indented content has a selStart of 1 ;\
-  setTimeout(function(){ // avoid race condition..
-    if(call.rep.selStart[1] === 0){
-      if(clientVars.plugins.plugins.ep_wrap.enabled){
+  setTimeout(() => { // avoid race condition..
+    if (call.rep.selStart[1] === 0) {
+      if (clientVars.plugins.plugins.ep_wrap.enabled) {
         // Thsi is a bit jerky but it works..  Resolves https://github.com/JohnMcLear/ep_wrap/issues/3
-        $('iframe[name="ace_outer"]').contents().find("#outerdocbody").scrollLeft(0);
+        $('iframe[name="ace_outer"]').contents().find('#outerdocbody').scrollLeft(0);
       }
     }
   }, 250);
-}
+};
 
 // When ACE initializes
-var postAceInit = function(hook, context){
+var postAceInit = function (hook, context) {
   wrap = {
-    status: function(){
+    status() {
       return $('#options-wrap').is(':checked');
     },
-    updateUI: function(){
+    updateUI() {
       // this is all bad.  This should be done on an edit event.
-      var maxWidth = 0;
-      var $innerdoc = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody");
-      var $outerdoc = $('iframe[name="ace_outer"]').contents().find("#outerdocbody");
+      let maxWidth = 0;
+      const $innerdoc = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody');
+      const $outerdoc = $('iframe[name="ace_outer"]').contents().find('#outerdocbody');
 
-      $innerdoc.find("div").each(function(){ // for each div
-        var divWidth = 0;
-        $(this).children().each(function(){ // for each span
-          var spanWidth = $(this).context.offsetWidth;
-          divWidth = divWidth + spanWidth; // get the div total width
+      $innerdoc.find('div').each(function () { // for each div
+        let divWidth = 0;
+        $(this).children().each(function () { // for each span
+          const spanWidth = $(this).context.offsetWidth;
+          divWidth += spanWidth; // get the div total width
         });
-        if(divWidth >= maxWidth){
+        if (divWidth >= maxWidth) {
           maxWidth = divWidth; // get the maximum width
         }
       });
-      $outerdoc.css({"overflow":"scroll", "width":maxWidth});
-      maxWidth = maxWidth+10;
-      $('iframe[name="ace_outer"]').contents().find('iframe').css("cssText", "min-width:"+maxWidth + "px !important");  //applies to ace_inner
+      $outerdoc.css({overflow: 'scroll', width: maxWidth});
+      maxWidth += 10;
+      $('iframe[name="ace_outer"]').contents().find('iframe').css('cssText', `min-width:${maxWidth}px !important`); // applies to ace_inner
     },
-    enable: function(){ // enables the line wrap functionality (this is the defualt behavior)
-      var $innerdoc = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody");
-      var $outerdoc = $('iframe[name="ace_outer"]').contents().find("#outerdocbody");
+    enable() { // enables the line wrap functionality (this is the defualt behavior)
+      const $innerdoc = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody');
+      const $outerdoc = $('iframe[name="ace_outer"]').contents().find('#outerdocbody');
 
-      $('iframe[name="ace_outer"]').contents().find('iframe').css("width","auto");
+      $('iframe[name="ace_outer"]').contents().find('iframe').css('width', 'auto');
 
       $innerdoc.addClass('doesWrap');
       // $outerdoc.css({"overflow":"hidden", "width":"auto"}); /* Breaks Firefox scrolling */
-      $('iframe[name="ace_outer"]').contents().find('iframe').removeAttr("style");  //applies to ace_inner
+      $('iframe[name="ace_outer"]').contents().find('iframe').removeAttr('style'); // applies to ace_inner
 
       clientVars.plugins.plugins.ep_wrap.enabled = false;
 
       // hide the popup dialogue
       padeditbar.toggleDropDown();
     },
-    disable: function(){ // disable the line wrap functionality
-      var $innerdoc = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody");
-      var $outerdoc = $('iframe[name="ace_outer"]').contents().find("#outerdocbody");
+    disable() { // disable the line wrap functionality
+      const $innerdoc = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody');
+      const $outerdoc = $('iframe[name="ace_outer"]').contents().find('#outerdocbody');
       $innerdoc.removeClass('doesWrap');
       // hide the popup dialogue
       padeditbar.toggleDropDown();
@@ -78,37 +78,35 @@ var postAceInit = function(hook, context){
       clientVars.plugins.plugins.ep_wrap.enabled = true;
       wrap.updateUI();
     },
-    getParam: function(sname)
-    {
-      var params = location.search.substr(location.search.indexOf("?")+1);
-      var sval = "";
-      params = params.split("&");
+    getParam(sname) {
+      let params = location.search.substr(location.search.indexOf('?') + 1);
+      let sval = '';
+      params = params.split('&');
       // split param and value into individual pieces
-      for (var i=0; i<params.length; i++)
-      {
-        temp = params[i].split("=");
-        if ( [temp[0]] == sname ) { sval = temp[1]; }
+      for (let i = 0; i < params.length; i++) {
+        temp = params[i].split('=');
+        if ([temp[0]] == sname) { sval = temp[1]; }
       }
       return sval;
-    }
-  }
+    },
+  };
   /* init */
-  if($('#options-wrap').is(':checked')) {
+  if ($('#options-wrap').is(':checked')) {
     wrap.enable();
   } else {
     wrap.disable();
   }
-  var urlContainswrapTrue = (wrap.getParam("wrap") == "true"); // if the url param is set
-  if(urlContainswrapTrue){
-    $('#options-wrap').attr('checked','checked');
+  const urlContainswrapTrue = (wrap.getParam('wrap') == 'true'); // if the url param is set
+  if (urlContainswrapTrue) {
+    $('#options-wrap').attr('checked', 'checked');
     wrap.enable();
-  }else if (wrap.getParam("wrap") == "false"){
-    $('#options-wrap').attr('checked',false);
+  } else if (wrap.getParam('wrap') == 'false') {
+    $('#options-wrap').attr('checked', false);
     wrap.disable();
-  } 
+  }
   /* on click */
-  $('#options-wrap').on('click', function() {
-    if($('#options-wrap').is(':checked')) {
+  $('#options-wrap').on('click', () => {
+    if ($('#options-wrap').is(':checked')) {
       wrap.enable(); // enables line wrapping
     } else {
       wrap.disable(); // disables line wrapping
--- wrap.js
+++ wrap.js
@@ -1,14 +1,13 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
-var settings = require('ep_etherpad-lite/node/utils/Settings');
-var checked_state = '';
+const eejs = require('ep_etherpad-lite/node/eejs/');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
+let checked_state = '';
 
 exports.eejsBlock_mySettings = function (hook_name, args, cb) {
   if (!settings.ep_wrap_default) checked_state = 'checked';
-  args.content = args.content + eejs.require('ep_wrap/templates/wrap_entry.ejs', {checked : checked_state});
+  args.content += eejs.require('ep_wrap/templates/wrap_entry.ejs', {checked: checked_state});
   return cb();
-}
+};
 
-exports.eejsBlock_styles = function (hook_name, args, cb)
-{
-  args.content = args.content + '<link href="../static/plugins/ep_wrap/static/css/wrap.css" rel="stylesheet">';
-} 
+exports.eejsBlock_styles = function (hook_name, args, cb) {
+  args.content += '<link href="../static/plugins/ep_wrap/static/css/wrap.css" rel="stylesheet">';
+};
